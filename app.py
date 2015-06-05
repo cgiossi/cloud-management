fe1ebcd50ab0e408170f3a10e0bd6d881367b3b4
Chris Giossi <cgiossi@pdx.edu>
2015-06-05 14:17:27 -0700

    Begin fixing query c to actually execute query c

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import boto.sdb
 import queryc
 
 def call_query(conn):
-    queryc.run(conn)
+    queryc.run(conn, 'Foster_NB')
 
 conn = boto.sdb.connect_to_region('us-west-2')
 call_query(conn)
--- queryc.py
+++ queryc.py
@@ -1,28 +1,27 @@
 import boto.sdb, datetime
 import tools
 
-def run(conn):
-    stationLengthMapping = {}
+def run(conn, station):
     timeInterval = datetime.timedelta(0,0,0,0,5)
-    domains = tools.find_teamb(conn)
-    #domains is just a list of strings now, so we'll have to grab individual
-    #domains through the connection
-    for d in domains:
-        #Grab domain from sdb to make queries against
-        query = 'SELECT direction FROM `' + d.name + '`'
-        results = d.select(query, max_items=1)
-        #Remove all domains that don't correspond to a NB station
-        if results.next()['direction'] != "NORTH":
-            print "Removing southbound domain from domains to query"
-            domains.remove(d)
-    for domain in domains:
-        print "Querying " + domain.name
-        query = 'SELECT stationlength FROM `' + domain.name + '`'
-        results = domain.select(query, max_items=1)
     startTime = datetime.datetime.strptime("2011-09-22 00:00:00", '%Y-%m-%d %H:%M:%S')
     endTime = datetime.datetime.strptime("2011-09-22 00:05:00", '%Y-%m-%d %H:%M:%S')
-    while startTime < datetime.strptime("2011-09-23 00:00:00", '%Y-%m-%d %H:%M:%S'):
-        query = 'SELECT speed FROM `' + domain.name +'` WHERE starttime between "' + \
-                startTime.strftime('%Y-%m-%d %H:%M:%s') + '" AND "' + \
-                endTime.strftime('%Y-%m-%d %H:%M:%s') + '"'
-        
+    print startTime.strftime('%Y-%m-%d %H:%M:%S')
+
+    stationDomain = conn.get_domain('TEAMB_' + station)
+
+    query = 'SELECT direction FROM `' + stationDomain.name + '`'
+    results = stationDomain.select(query, max_items=1)
+
+    print "Querying " + stationDomain.name + " for station length"
+    query = 'SELECT length FROM `' + stationDomain.name + '`'
+    results = stationDomain.select(query, max_items=1)
+    stationLength = results.next()['length']
+
+    while startTime < datetime.datetime.strptime("2011-09-23 00:00:00", '%Y-%m-%d %H:%M:%S'):
+        sTime = startTime.strftime('%Y-%m-%d %H:%M:%S-07')
+        eTime = endTime.strftime('%Y-%m-%d %H:%M:%S-07')
+        query = 'SELECT detectorid, speed, starttime FROM `' + stationDomain.name + '`'
+        results = stationDomain.select(query)
+        for i in results:
+            print i
+
